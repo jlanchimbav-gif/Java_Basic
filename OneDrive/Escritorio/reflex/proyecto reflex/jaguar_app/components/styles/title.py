5678de58f47d074320a5ca3b9328ff32bc1e4fb4
Jaguar Dev <dev@jaguar-web.local>
2026-05-06 17:08:49 -0500

    main

--- OneDrive/Escritorio/reflex/proyecto reflex/jaguar_app/components/styles/title.py
+++
@@ -1,8 +0,0 @@
-import reflex as rx
-import jaguar_app.components.styles.styles as styles
-
-def title(text: str) -> rx.Component:
-    return rx.heading(
-        text,
-        **styles.title_styles,
-    )
