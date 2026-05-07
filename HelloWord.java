23b8f3f35f7a1f00fb5ab080ab70f1bd8b1df12a
Jaguar Dev <dev@jaguar-web.local>
2026-05-06 23:56:32 -0500

    main

---
+++ HelloWord.java
@@ -0,0 +1,10 @@
+/* Hola Java */
+/*Clase 1 */
+/*Autor: Jaguar EW*/
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hola Java");
+    }
+}
+
---
+++ HelloWordExercicies.java
@@ -0,0 +1,19 @@
+/* Hello Word Exercises */
+
+public class HelloWordExercicies {
+
+ /* imprimir un saludo con tu nombre y tu ciudad*/
+    public static void main(String[] args) {
+        String nombre = "Jaguar EW";
+        String ciudad = "Quito, Ecuador";
+/* crear comentario en varias lineas el nombre y la ciudad */
+        System.out.println("Hola, me llamo " + nombre + " y vivo en " + ciudad);
+        System.out.println("Hola, me llamo " + nombre);
+        System.out.println("y vivo en " + ciudad);
+/* imprimir un diseño ASCII */
+        System.out.println("  /\\_/\\  ");
+        System.out.println(" ( o.o ) ");
+        System.out.println("  > ^ <  ");
+    }
+
+}
