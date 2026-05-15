305fcc88e4f46cc4bc646545530fb9aedf21d01d
Jaguar Dev <dev@jaguar-web.local>
2026-05-14 22:47:18 -0500

    main

--- structures/Arrays.java
+++ structures/Arrays.java
@@ -1,5 +1,27 @@
 package structures;
 
 public class Arrays {
-    
+    public static void main(String[] args) {
+    /* Un arreglo es una estructura de datos que almacena una colección de elementos del mismo tipo.
+    }
+    /* Declaracion y creacion de arreglos */
+int[] numbers = new int[5]; // Arreglo de enteros con 5 elementos
+     System.out.println(numbers[0]);
+     
+String[] names ={"Alejandro", "Jaguar", "JaguarDev"}; // Arreglo de cadenas con 3 elementos
+     System.out.println(names[0]); // Imprime el primer elemento del arreglo
+
+     /* acceso a elementos del arreglo */
+     System.out.println(numbers[2]); // Imprime el tercer elemento del arreglo
+     System.out.println(names[1]); // Imprime el segundo elemento del arreglo
+
+        /* Modificacion de elementos del arreglo */
+
+        numbers[0] = 10; // Asigna el valor 10 al primer elemento del arreglo
+        names[1] = "Pantera"; // Asigna el valor "Pantera" al segundo elemento del arreglo
+        System.out.println(numbers[0]); // Imprime el primer elemento del arreglo
+        System.out.println(names[1]); // Imprime el segundo elemento del arreglo
+
+
+}
 }
