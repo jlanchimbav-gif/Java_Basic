b8d83dba13ad826a4e41d1387df1db53fe78f385
Jaguar Dev <dev@jaguar-web.local>
2026-05-27 16:58:40 -0500

    Fix Java compilation issues

---
+++ Java Proyect/Java Proyect/Sistema-gestion-biblioteca/Clases/Libro.java
@@ -0,0 +1,22 @@
+public class Libro {
+    private String titulo;
+    private String autor;
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" + "titulo='" + titulo + '\'' + ", autor='" + autor + '\'' + '}';
+    }
+}
